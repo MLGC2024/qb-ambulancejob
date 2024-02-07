74341fc66bc5906db03a45e7e245bec17f8ba0bf
GG79 <midlifegamingcrisis@gmail.com>
2024-02-07 21:46:32 +0200

    added trigger for wais battlepass

--- client/wounding.lua
+++ client/wounding.lua
@@ -50,6 +50,7 @@ RegisterNetEvent('hospital:client:UseIfaks', function()
         TriggerServerEvent('hospital:server:removeIfaks')
         TriggerEvent('inventory:client:ItemBox', QBCore.Shared.Items['ifaks'], 'remove')
         TriggerServerEvent('hud:server:RelieveStress', math.random(12, 24))
+        TriggerEvent('wais:addmissionxp:useifaks', 1) --added by pamela for wais battlepass
         SetEntityHealth(ped, GetEntityHealth(ped) + 10)
         if painkillerAmount < 3 then
             painkillerAmount = painkillerAmount + 1
